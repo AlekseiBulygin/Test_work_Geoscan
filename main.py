3ee151f82fbdbd26cb73524f5d4df5d3f0559307
Aleksei Bulygin <eksuby@rambler.ru>
2019-02-05 23:00:00 +0300

    add pour water add find border

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainWindow(QMainWindow):
         qp = QPainter()
         qp.begin(self)
         self.build_towers(qp)
+        self.pour_water(qp)
         qp.end()
 
     def build_towers(self, qp):
@@ -30,8 +31,50 @@ class MainWindow(QMainWindow):
                 qp.setBrush(QColor(153, 153, 102))
                 qp.drawRect(10 + j*40, 380 - 40*i, 40, 40)
 
-    def pour_water(self):
-        pass
+    def find_border(self):
+        left_side = 0
+        left_index = 0
+        right_side = 0
+        right_index = 0
+        bottom = 10
+        bottom_index = 0
+        for i in range(len(self.towers)):
+            print('\t', i)
+            if self.towers[i] == 0 and right_side == 0:
+                left_side = 0
+                left_index = 0
+                right_side = 0
+                right_index = 0
+                bottom = 10
+                bottom_index = 0
+            elif self.towers[i] > 1 and self.towers[i] > left_side and bottom == 10:
+                left_side = self.towers[i]
+                left_index = i
+                print(left_side, left_index)
+            elif self.towers[i] < left_side and self.towers[i] < bottom:
+                bottom = self.towers[i]
+                bottom_index = i
+            elif self.towers[i] > bottom and self.towers[i] > right_side:
+                right_side = self.towers[i]
+                right_index = i
+
+        if left_side != 0 and right_side != 0 and bottom != 10:
+            return (min(left_side, right_side), left_index, right_index, bottom)
+
+    def pour_water(self, qp):
+        color = QColor(51, 204, 255)
+        qp.setPen(color)
+        result = self.find_border()
+        print(result)
+        deep = result[0]
+        start = result[1]
+        finish = result[2]
+        for j in range(len(self.towers))[start+1:finish+1]:
+            for i in range(deep)[self.towers[j]:]:
+                qp.setBrush(QColor(51, 204, 255))
+                qp.drawRect(10 + j*40, 380 - 40*i, 40, 40)
+
+
 
 
 def main(args):
