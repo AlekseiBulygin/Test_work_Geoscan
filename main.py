7c5a3eff22b6a4537d074c48f8157812c5f5bd8d
Aleksei Bulygin <eksuby@rambler.ru>
2019-02-06 12:36:57 +0300

    correct find_border function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.towers = args
         self.setupUi()
-
+        self.cups = []
         print(args)
 
     def setupUi(self):
@@ -37,44 +37,55 @@ class MainWindow(QMainWindow):
         right_side = 0
         right_index = 0
         bottom = 10
-        bottom_index = 0
+
         for i in range(len(self.towers)):
-            print('\t', i)
-            if self.towers[i] == 0 and right_side == 0:
+            if self.towers[i] != 0:
+                if self.towers[i] > 1 and self.towers[i] > left_side and bottom == 10:
+                    left_side = self.towers[i]
+                    left_index = i
+                elif self.towers[i] < left_side and self.towers[i] < bottom:
+                    bottom = self.towers[i]
+                elif self.towers[i] > bottom and self.towers[i] >= right_side:
+                    right_side = self.towers[i]
+                    right_index = i
+
+                if left_side != 0 and right_side != 0 and bottom != 10 and right_side >= left_side:
+                    tup = (min(left_side, right_side), left_index, right_index, bottom)
+                    if tup not in self.cups:
+                        self.cups.append(tup)
+                        left_side, right_side = right_side, 0
+                        left_index, right_index = right_index, 0
+                        bottom = 10
+            elif self.towers[i] == 0:
+                if left_side != 0 and right_side != 0 and bottom != 10:
+                    tup = (min(left_side, right_side), left_index, right_index, bottom)
+                    if tup not in self.cups:
+                        self.cups.append(tup)
                 left_side = 0
                 left_index = 0
                 right_side = 0
                 right_index = 0
                 bottom = 10
-                bottom_index = 0
-            elif self.towers[i] > 1 and self.towers[i] > left_side and bottom == 10:
-                left_side = self.towers[i]
-                left_index = i
-                print(left_side, left_index)
-            elif self.towers[i] < left_side and self.towers[i] < bottom:
-                bottom = self.towers[i]
-                bottom_index = i
-            elif self.towers[i] > bottom and self.towers[i] > right_side:
-                right_side = self.towers[i]
-                right_index = i
 
         if left_side != 0 and right_side != 0 and bottom != 10:
-            return (min(left_side, right_side), left_index, right_index, bottom)
+            tup = (min(left_side, right_side), left_index, right_index, bottom)
+            if tup not in self.cups:
+                self.cups.append(tup)
 
     def pour_water(self, qp):
+        cups = self.cups
         color = QColor(51, 204, 255)
         qp.setPen(color)
-        result = self.find_border()
-        print(result)
-        deep = result[0]
-        start = result[1]
-        finish = result[2]
-        for j in range(len(self.towers))[start+1:finish+1]:
-            for i in range(deep)[self.towers[j]:]:
-                qp.setBrush(QColor(51, 204, 255))
-                qp.drawRect(10 + j*40, 380 - 40*i, 40, 40)
-
-
+        self.find_border()
+        print(cups)
+        for result in cups:
+            deep = result[0]
+            start = result[1]
+            finish = result[2]
+            for j in range(len(self.towers))[start+1:finish+1]:
+                for i in range(deep)[self.towers[j]:]:
+                    qp.setBrush(QColor(51, 204, 255))
+                    qp.drawRect(10 + j*40, 380 - 40*i, 40, 40)
 
 
 def main(args):
